ed67a041250e30f2e4e990b01403198b3fe3b8ab
vivishno <64971172+vivishno@users.noreply.github.com>
2020-07-14 16:03:03 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -18,8 +18,11 @@ def main():
     deployment_mode=os.environ.get("INPUT_DEPLOYMENT_MODE", default="Incremental")
     deployment_name=os.environ.get("GITHUB_REPOSITORY")
     print(resource_group)
-    print("asdasdasdasddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd-----")
-    print(resource_group[0])
+    print("-----------------------------------------------------------d-----")
+    tst=resource_group.split('\n')
+    print(tst)
+    print("----------------------")
+    print(tst[0])
     deployment_name=deployment_name.split('/')
     deployment_name=deployment_name[0]+'_'+deployment_name[1]
     
