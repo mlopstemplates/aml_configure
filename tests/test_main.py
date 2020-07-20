a10189540ec6386532bc4def6285b18e7cc17097
vivishno <64971172+vivishno@users.noreply.github.com>
2020-07-21 01:07:54 +0530

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -17,47 +17,10 @@ def get_sample_credentials():
         'tenantId': 'test'
     }"""  
 
-
-def test_main_no_input():
-    """
-    Unit test to check the main function with no inputs
-    """
-    with pytest.raises(AMLConfigurationException):
-        assert main()
-
-
-def test_main_invalid_azure_credentials():
-    os.environ["INPUT_AZURE_CREDENTIALS"] = ""
-    with pytest.raises(AMLConfigurationException):
-        assert main()
-
-def test_main_resource_grp_not_provided():
-    os.environ["INPUT_AZURE_CREDENTIALS"] = get_sample_credentials()
-    with pytest.raises(AMLConfigurationException):
-        assert main()
-        
-def test_main_invalid_mapped_parameters():
-    os.environ["INPUT_AZURE_CREDENTIALS"] = get_sample_credentials()
-    os.environ["INPUT_MAPPED_PARAMS"] ="wrong mapped params"
-    with pytest.raises(AMLConfigurationException):
-        assert main()        
-        
-def test_main_invalid_parameters_file():
-    os.environ["INPUT_AZURE_CREDENTIALS"] =get_sample_credentials()
-    os.environ["INPUT_PARAMETERS_FILE"] = "wrongfile.json"
-    with pytest.raises(AMLConfigurationException):
-        assert main()
-
-def test_main_invalid_parameters_file():        
-    os.environ["INPUT_AZURE_CREDENTIALS"] =get_sample_credentials()
-    os.environ["INPUT_RESOURCE_GROUP"] = "testGroup"
-    with pytest.raises(CredentialsVerificationError):
-        assert main()
-
-
 def test_main_invalid_parameters_files(mocker):        
     os.environ["INPUT_AZURE_CREDENTIALS"] =get_sample_credentials()
     os.environ["INPUT_RESOURCE_GROUP"] = "testGroup"
+    mocker.patch('utils.get_service_principal_credentials', return_value=None)
     with pytest.raises(ResourceManagementError):
       assert main()
     
