dc9f567a813e347431a2b9d8f0d144f671082d8f
vivishno <64971172+vivishno@users.noreply.github.com>
2020-07-21 02:04:06 +0530

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -62,7 +62,7 @@ def test_main_invalid_parameters_files(mocker):
     os.environ["INPUT_AZURE_CREDENTIALS"] =get_sample_credentials()
     os.environ["INPUT_RESOURCE_GROUP"] = "testGroup"
     os.environ["INPUT_MAPPED_PARAMS"] ='{"testParams":"testValue"}'
-    mocker.patch('utils.get_service_principal_credentials', return_value=None)
+    mocker.patch('get_service_principal_credentials', return_value=None)
     with pytest.raises(ResourceManagementError):
       assert main()
     
