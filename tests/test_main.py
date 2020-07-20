bc49c85f17f919c817973ddefc6e7a02e78b8bfe
vivishno <64971172+vivishno@users.noreply.github.com>
2020-07-21 01:06:33 +0530

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,7 +7,7 @@ myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(myPath, "..", "code"))
 
 from main import main
-from utils import AMLConfigurationException, CredentialsVerificationError, get_service_principal_credentials
+from utils import AMLConfigurationException, CredentialsVerificationError
 
 def get_sample_credentials():
     return """{
@@ -58,7 +58,6 @@ def test_main_invalid_parameters_file():
 def test_main_invalid_parameters_files(mocker):        
     os.environ["INPUT_AZURE_CREDENTIALS"] =get_sample_credentials()
     os.environ["INPUT_RESOURCE_GROUP"] = "testGroup"
-    mocker.patch('utils.get_service_principal_credentials', return_value="hello")
     with pytest.raises(ResourceManagementError):
       assert main()
     
