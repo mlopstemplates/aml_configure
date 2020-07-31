cbfa4b4c3bbe1f69f7a00598b0cf719bee8528b7
vivishno <64971172+vivishno@users.noreply.github.com>
2020-07-31 13:28:21 +0530

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -61,8 +61,7 @@ def test_main_invalid_credentials():
         
 @mock.patch("main.ServicePrincipalCredentials",return_value="check3",autospec=True)
 @mock.patch("main.validate",return_value="check4",autospec=True)
-@mock.patch("main.wait",return_value="check5",autospec=True)
-def test_main_template_file_not_provided(mock_check,mock_check2,mock_check3): 
+def test_main_template_file_not_provided(mock_check,mock_check2): 
     os.environ["INPUT_AZURE_CREDENTIALS"] =get_sample_credentials()
     os.environ["INPUT_MAPPED_PARAMS"] ='{"testParams":"testValue"}'
     os.environ["INPUT_RESOURCE_GROUP"] = "testGroup"
