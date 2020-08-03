ab137f7737a6a4b5bbe87085c64a5d7ec086ef44
vivishno <64971172+vivishno@users.noreply.github.com>
2020-08-03 18:15:24 +0530

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -69,14 +69,6 @@ def test_main_validation_fails(mock_check):
         
 
 @mock.patch("main.ServicePrincipalCredentials",return_value="check3",autospec=True)
-def test_main_validation_fails(mock_check): 
-    os.environ["INPUT_AZURE_CREDENTIALS"] =get_sample_credentials()
-    os.environ["INPUT_MAPPED_PARAMS"] ='{"testParams":"testValue"}'
-    os.environ["INPUT_RESOURCE_GROUP"] = "testGroup"    
-    with pytest.raises(ActionDeploymentError):
-        assert main()  
-
-@mock.patch("main.ServicePrincipalCredentials",return_value="check3",autospec=True)
 def test_main_invalid_template_file_provided(mock_check): 
     os.environ["INPUT_AZURE_CREDENTIALS"] =get_sample_credentials()
     os.environ["INPUT_MAPPED_PARAMS"] ='{"testParams":"testValue"}'
