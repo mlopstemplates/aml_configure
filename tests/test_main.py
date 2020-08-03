545b35d96a11d86292bce119418cfd0ff8814c84
vivishno <64971172+vivishno@users.noreply.github.com>
2020-08-03 18:47:39 +0530

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -64,13 +64,6 @@ def test_main_template_file_not_provided(mock_check):
     os.environ["INPUT_AZURE_CREDENTIALS"] =get_sample_credentials()
     os.environ["INPUT_MAPPED_PARAMS"] ='{"testParams":"testValue"}'
     os.environ["INPUT_RESOURCE_GROUP"] = "testGroup"
+    os.environ["INPUT_ARMTEMPLATE_FILE"] = "InvalidFile.json"
     with pytest.raises(ActionDeploymentError):
        assert main()
-
-@mock.patch("main.ServicePrincipalCredentials",return_value="check4",autospec=True)
-def test_main_template_file_not_provided(mock_check2): 
-    os.environ["INPUT_AZURE_CREDENTIALS"] =get_sample_credentials()
-    os.environ["INPUT_MAPPED_PARAMS"] ='{"testParams":"testValue"}'
-    os.environ["INPUT_RESOURCE_GROUP"] = "testGroup"
-    os.environ["INPUT_ARMTEMPLATE_FILE"] = "InvalidFile.json" 
-   
